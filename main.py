7b4fa76466841c0a41e72dc6f89ce65a97eeccc0
Stephen Easley-Walsh <stephen.easley-walsh@studygroup.com>
2026-05-12 12:35:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-x = int(input("Give me a number from 1 to 100 but it must be even: "))
+x = int(input("Give me an EVEN integer from 1 to 100: "))
 
-if x >= 1 and x <= 100 and x%2 == 0:
-    print("thank you")
+if x < 1:
+    print("That is too small, error!!!")
+elif x > 100:
+    print("That is too big, error!!!")
+elif x%2 == 1: # Remainder is 1 not 0
+    print("That is ODD not even, error!!!")
 else:
-    print("That is not correct")
+    print("Thank you, that is EVEN and from 1 to 100")
